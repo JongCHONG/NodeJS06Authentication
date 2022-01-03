f9813caa0ac9b7fda3d19c6c1b5e0a109f0baef7
JongCHONG <yonhoa27@hotmail.fr>
2022-01-03 16:22:39 +0100

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Admin from './pages/Admin'
+import Login from './pages/Login'
+import NotFound from'./pages/NotFound'
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div className="container my-2">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
