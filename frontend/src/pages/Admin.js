a4f86cc7641109664526ce5d00bfe7b4cc4377b7
unknown <jochong27@gmail.com>
2022-01-03 22:34:33 +0100

    message erreur

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Admin = () => {
+  const [users, setUsers] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:5000/admin')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  },[])
+
   return (
     <div>
       <h1>Admin</h1>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 
 import { useFormik } from 'formik'
 
 const Login = () => {
+  // const navigate = useNavigate()
+  const [error, setError] = useState(null)
 
   const formik = useFormik({
     initialValues: {
@@ -10,10 +13,10 @@ const Login = () => {
       password: ""
     },
     onSubmit: values => {
-
       const User = {
         ...values
       }
+
       fetch('http://localhost:5000/auth/login',{
         method: "post",
         headers: {
@@ -23,14 +26,26 @@ const Login = () => {
         body: JSON.stringify(User)
       })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(
+          data => {
+            console.log(data)
+            setError("")
+          },
+          err => {
+            setError(err)
+          }
+        )
+        // .then(
+        //   // () => navigate('/admin')
+        // ) 
     }
   })
   
-  console.log(formik.values)
+  // console.log(formik.values)
   return (
     <div>
       <h1>Login</h1>
+      {error && "User Not Found"}
       <form onSubmit={formik.handleSubmit}>
         <div className="mb-3">
           <label className="form-label">Username</label>
